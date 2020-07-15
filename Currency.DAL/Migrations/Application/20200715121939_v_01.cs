fb2abb0570463131d125393d321081cd136534f9
Владимир Шарапов <war-beast@yandex.ru>
2020-07-15 15:24:03 +0300

    Добавлена начальная миграция БД курсов валют

---
+++ Currency.DAL/Migrations/Application/20200715121939_v_01.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CurrencyApp.DAL.Migrations.Application
+{
+    public partial class v_01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Rate = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Currencies");
+        }
+    }
+}
