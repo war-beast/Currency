0da98326dd6c15da3fc910f51b8038dd68980a43
Владимир Шарапов <war-beast@yandex.ru>
2020-07-22 09:30:02 +0300

    Добавлено хранилище vuex

---
+++ Currency/Src/modules/store.ts
@@ -0,0 +1,17 @@
+﻿import Vue from "vue";
+import Vuex, { StoreOptions } from "vuex";
+import { RootState } from "Interfaces/profile/types";
+import { profile } from "Modules/profile/index";
+
+Vue.use(Vuex);
+
+const store: StoreOptions<RootState> = {
+	state: {
+		version: "1.0.0" // a simple property
+	},
+	modules: {
+		profile
+	}
+};
+
+export default new Vuex.Store<RootState>(store);
